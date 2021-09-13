c627e9dba5134cabf1e966ba38af3733b698374e
Iryna <iryna.shulim@gmail.com>
2021-09-12 23:21:59 -0500

    final updates

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,8 +5,8 @@ const thoughtController = {
   //get all thoughts
   getAllThoughts(req, res) {
     Thought.find({})
-      .populate({ path: "reactions", select: "-__v" })
       .select("-__v")
+      .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         res.status(400).json(err);
@@ -16,7 +16,6 @@ const thoughtController = {
   //get single thought by id
   getThoughtById({ params }, res) {
     Thought.findOne({ _id: params.id })
-      .populate({ path: "reactions", select: "-__v" })
       .select("-__v")
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
@@ -58,7 +57,7 @@ const thoughtController = {
 
   //update thought by id
   updateThought({ params, body }, res) {
-    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
           res.status(400).json({ message: "Thought ID does not exist" });
